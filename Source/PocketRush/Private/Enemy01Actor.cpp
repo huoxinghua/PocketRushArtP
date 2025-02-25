ab90a486c27a91942878bebb163783f0feb185d9
Xinghua <huoxinghuacn@gmail.com>
2025-02-25 14:16:14 -0700

    enemy2

---
+++ Source/PocketRush/Private/Enemy01Actor.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Enemy01Actor.h"
+
+// Sets default values
+AEnemy01Actor::AEnemy01Actor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AEnemy01Actor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+// Called every frame
+void AEnemy01Actor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+
+}
+
